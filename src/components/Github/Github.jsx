d2d74edbea8988cd0592c50496d9f50b885b159e
Qasim Farooq <qasimfarooq631@gmail.com>
2024-09-05 17:50:20 +0500

    commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    // fetch('https://api.github.com/users/Qasim567')
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data)
+    //     setData(data)
+    // })
+    // }, [])
+  return (
+    <div className='bg-gray-600 text-white text-3xl text-center p-4'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git Picture" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/Qasim567')
+    return response.json()
+}
